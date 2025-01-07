f2fc08c2362abcd12e2bb38e40324279a938d718
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-08 01:47:37 +0800

    adding user pages setup

---
+++ client/src/pages/user/index.js
@@ -0,0 +1,4 @@
+export { default as DashboardUser } from './DashboardUser'
+export { default as ProfilUser } from './ProfilUser'
+export { default as PengajuanUser } from './PengajuanUser'
+export { default as StatusPengajuanUser } from './StatusPengajuanUser'
