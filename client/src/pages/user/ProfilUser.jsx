61a4d37063732ba97315cdcb2e2c85132d473814
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-09 01:33:56 +0800

    setup profile page

--- client/src/pages/user/ProfilUser.jsx
+++ client/src/pages/user/ProfilUser.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useUserDashboardContext } from './DashboardUser'
 
 export const loader = async() => {
   return null
 }
 
 const ProfilUser = () => {
+
+  const { user } = useUserDashboardContext()
+
   return (
-    <div>ProfilUser</div>
+    <section className='w-full h-full overflow-y-auto p-10 flex items-center justify-center flex-col'>
+
+    {/* welcome sign */}
+    <section className='w-full h-full rounded-xl p-4'>
+      <h1 className='text-4xl font-semibold text-slate-900'>Hallo, {user.nama} ✨</h1>
+      <p className='text-md w-[80%] mt-2 text-slate-500'>Selamat datang di halaman profil Anda. Di sini, Anda dapat melihat detail informasi pribadi dan memperbaruinya jika diperlukan.</p>
+
+    </section>
+
+
+  </section>
   )
 }
 
