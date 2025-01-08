eed34e89cc205c03514f0862467a7b3a914ff22a
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-09 04:00:56 +0800

    add data for chaining

--- client/src/utils/constant.jsx
+++ client/src/utils/constant.jsx
@@ -17,3 +17,87 @@ export const sidebarLinks = [
         path: 'profil'
     },
 ]
+
+export const baubauData = [
+    {
+      kecamatan: "Batupoaro",
+      kelurahan: [
+        "Baadia",
+        "Lamangga",
+        "Melai",
+        "Tanganapada",
+        "Wajo",
+      ],
+    },
+    {
+      kecamatan: "Betoambari",
+      kelurahan: [
+        "Betoambari",
+        "Katobengke",
+        "Sulaa",
+        "Nganganaumala",
+        "Tanganapada",
+      ],
+    },
+    {
+      kecamatan: "Bungi",
+      kelurahan: [
+        "Bungi",
+        "Karing-Karing",
+        "Waborobo",
+        "Wabula",
+      ],
+    },
+    {
+      kecamatan: "Kokalukuna",
+      kelurahan: [
+        "Kokalukuna",
+        "Liwuto",
+        "Sukanayo",
+        "Wale",
+        "Tomba",
+        "Wangkanapi",
+      ],
+    },
+    {
+      kecamatan: "Lea-Lea",
+      kelurahan: [
+        "Lea-Lea",
+        "Kantalai",
+        "Gonda Baru",
+        "Waborobo",
+      ],
+    },
+    {
+      kecamatan: "Murhum",
+      kelurahan: [
+        "Melai",
+        "Nganganaumala",
+        "Tanganapada",
+        "Wajo",
+        "Ngkaring-Ngkaring",
+      ],
+    },
+    {
+      kecamatan: "Sorawolio",
+      kelurahan: [
+        "Sorawolio",
+        "Karing-Karing",
+        "Waborobo",
+        "Wabula",
+      ],
+    },
+    {
+      kecamatan: "Wolio",
+      kelurahan: [
+        "Bataraguru",
+        "Batulo",
+        "Bukit Wolio Indah",
+        "Kadolokatapi",
+        "Tomba",
+        "Wale",
+        "Wangkanapi",
+      ],
+    },
+  ];
+  
