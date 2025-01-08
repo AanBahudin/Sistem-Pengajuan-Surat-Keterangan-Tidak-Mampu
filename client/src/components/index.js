3a5531c21e5aaaf4a4fdd2f5b8bbacd0a103a91c
Aan Bahudin <aan.bahudin11@gmail.com>
2025-01-09 00:29:29 +0800

    createing formInput component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as CustomToast } from './CustomToast'
+export { default as FormInput } from './FormInput'
 export { default as Sidebar } from './Sidebar'
 export { default as SidebarLink } from './SidebarLink'
